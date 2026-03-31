5bbdb2625d0c4ce655d6d74735e38baeb2c071bc
Abdo ben yousef <abdomenyousef@gmail.com>
2026-04-01 01:08:25 +0200

    epic2

--- alembic/versions/0d52ce81312c_massage.py
+++ alembic/versions/0d52ce81312c_massage.py
@@ -5,17 +5,14 @@ Revises: 9c1f6e2a4b11
 Create Date: 2026-03-31 22:56:09.933778
 
 """
-from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
+from collections.abc import Sequence
 
 # revision identifiers, used by Alembic.
-revision: str = '0d52ce81312c'
-down_revision: Union[str, None] = '9c1f6e2a4b11'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision: str = "0d52ce81312c"
+down_revision: str | None = "9c1f6e2a4b11"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
